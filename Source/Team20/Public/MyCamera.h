8b5d2d23cb7c762f48284426c1bc88db36c09b81
Minha Lee <fgz1234@gmail.com>
2023-08-30 20:12:22 +0900

    commit

---
+++ Source/Team20/Public/MyCamera.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MyCamera.generated.h"
+
+UCLASS()
+class TEAM20_API AMyCamera : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyCamera();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;public:
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+};
---
+++ Source/Team20/Public/WebSocketGameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "IWebSocket.h"
+#include "WebSocketGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM20_API UWebSocketGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void Init() override;
+	virtual void Shutdown() override;
+
+	TSharedPtr<IWebSocket> WebSocket;
+};
